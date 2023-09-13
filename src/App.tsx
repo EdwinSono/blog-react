44a4303f8cdfbd742a234592783892d0eda02dbc
Edwin Sono <edwinsono@gmail.com>
2023-09-12 21:54:48 -0500

    feat: add menu & pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import { UseState } from '../UseState';
-import { UseReducer } from './app/UseReducer';
-import { UseState } from './app/UseState';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Menu } from './Menu';
+import { HomePage } from './pages/HomePage';
+import { BlogPage } from './pages/BlogPage';
+import { ProfilePage } from './pages/ProfilePage';
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import { UseReducer } from './app/UseReducer';
+// import { UseState } from './app/UseState';
 // import { ClassState } from './ClassState';
 import './App.css'
 
-// import { HashRouter, Routes, Route } from 'react-router-dom';
-// import { Menu } from './Menu';
-// import { HomePage } from './HomePage';
-// import { BlogPage } from './BlogPage';
-// import { ProfilePage } from './ProfilePage';
-
-
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Practicando estados</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="App">
-        <div className="App-section"><UseState name="Use State" /></div>
-        {/* <div className="App-section"><ClassState name="Class State" /></div> */}
-        {/* <UseState name="Use State" /> */}
-        <div className="App-section"><UseReducer name="Use Reducer" /></div>
-      </div>
-    </>
+    <HashRouter>
+      <Menu />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<p>Not found</p>} />
+      </Routes>
+    </HashRouter>
+  </>
+    // <>
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Practicando estados</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <div className="App">
+    //     <div className="App-section"><UseState name="Use State" /></div>
+    //     {/* <div className="App-section"><ClassState name="Class State" /></div> */}
+    //     {/* <UseState name="Use State" /> */}
+    //     <div className="App-section"><UseReducer name="Use Reducer" /></div>
+    //   </div>
+    // </>
   )
 }
 
